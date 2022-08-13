bf461b122dbcabc7b3e07c2b757a387fa8f41a6b
utiyama <ninjin0604@gmail.com>
2022-08-13 14:44:20 +0900

    RecoilRootの追加とリセットcssの読み込み

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
-import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
 
+import { RecoilRoot } from "recoil";
+
+import "styles/global.css";
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
 };
 export default MyApp;
