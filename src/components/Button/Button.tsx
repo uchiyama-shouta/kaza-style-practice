f97ac45a47258c31d19c0b2b56fb07150c23228e
utiyama <ninjin0604@gmail.com>
2022-08-13 14:49:52 +0900

    margeStyleを試すためコンポーネント化

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,8 +2,38 @@ import type { ComponentProps, FC } from "react";
 
 type Props = ComponentProps<"button">;
 
+import { createStyle, margeStyle } from "@kaze-style/react";
+
+const classes = createStyle({
+  button: {
+    height: "40px",
+    paddingLeft: "8px",
+    paddingRight: "8px",
+    marginLeft: "10px",
+    borderTopStyle: "solid",
+    borderRightStyle: "solid",
+    borderBottomStyle: "solid",
+    borderLeftStyle: "solid",
+    borderTopColor: "#000",
+    borderRightColor: "#000",
+    borderBottomColor: "#000",
+    borderLeftColor: "#000",
+    borderTopWidth: "1px",
+    borderRightWidth: "1px",
+    borderBottomWidth: "1px",
+    borderLeftWidth: "1px",
+  },
+});
+
 const Button: FC<Props> = (props) => {
-  return <button className="text-lg text-red-500" {...props} />;
+  const { className, ...buttonProps } = props;
+
+  return (
+    <button
+      className={margeStyle(classes.button, className)}
+      {...buttonProps}
+    />
+  );
 };
 
 export default Button;
